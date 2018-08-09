99a9d13bf4555ae224465280857b11d11caf0525
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 13:45:46 +0300

    fd

--- lab/app.py
+++ lab/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', students=query_all())
+	return render_template('home.html', students=query_all())
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
-    return render_template('student.html', student=query_by_id(student_id))
+	return render_template('student.html', student=query_by_id(student_id))
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_student_route():
@@ -18,5 +18,19 @@ def add_student_route():
 		add_student(request.form['student_name'],
 			request.form['student_year'], False)
 		return render_template('add.html')
+
+
+
+
+
+@app.route('/delete/<int:student_id>', methods=['GET', 'POST'])
+def delete_student(student_id):
+	if request.method == 'POST':
+		delete_student(query_by_id(student_id).name) 
+		return 'You just deleted a student!'
+	else:
+
+		return render_template('student.html', student=query_by_id(student_id))
+
 
 app.run(debug=True)
